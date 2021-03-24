61802aa14d20023427d5c1809f900f8a1ad67026
Blaise Telfer <56236726+Blaise-Telfer@users.noreply.github.com>
2021-03-24 03:40:47 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { connect } from "react-redux";
 import jwt_decode from "jwt-decode";
 
 // Pages
